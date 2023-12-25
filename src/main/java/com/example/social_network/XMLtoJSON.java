222241fb2cc5c8b985c1aa4c7744d78f372efb37
SarahSamehh <132105101+SarahSamehh@users.noreply.github.com>
2023-12-25 22:58:58 +0200

    Update XMLtoJSON.java

--- src/main/java/com/example/social_network/XMLtoJSON.java
+++ src/main/java/com/example/social_network/XMLtoJSON.java
@@ -186,5 +186,54 @@ public class XMLtoJSON{
     }
 }
 
+//First Edit of Class User for Part 2
+/*public class User {
 
 
+    private String id;
+    private String name;
+    private List<String> posts;
+
+    public List<String> getPosts() { 
+        return posts;
+    }
+
+    public List<String> getFollowers() {
+        return followers;
+    }
+    private List<String> followers;
+
+    public User() { 
+        this.posts = new ArrayList<>();
+        this.followers = new ArrayList<>();
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    // Getters and setters
+
+    public void addPost(String post) {
+        this.posts.add(post);
+    }
+
+    public void addFollower(String followerId) {
+        this.followers.add(followerId);
+    }
+}
+ */
