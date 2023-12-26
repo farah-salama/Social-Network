b12d015e550c38c99f3f3d253849a76a437d9150
Jasmine <112454290+JasmineMamdouh@users.noreply.github.com>
2023-12-26 20:17:03 +0200

    Add files via upload

---
+++ src/main/java/com/example/social_network/Post.java
@@ -0,0 +1,42 @@
+package org.example.graphvisualization;
+
+import java.util.List;
+
+public class Post {
+    private String body;
+    private List<String> topics;
+
+    //constructor, setters and getters
+    public Post() {
+        this.body = null;
+        this.topics = null;
+
+    }
+
+    public Post(String body, List<String> topics) {
+        this.body = body;
+        this.topics = topics;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setTopics(List<String> topics) {
+        this.topics = topics;
+    }
+
+    public void addTopic(String topic) {
+        this.topics.add(topic);
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public List<String> getTopics() {
+        return topics;
+    }
+
+
+}
