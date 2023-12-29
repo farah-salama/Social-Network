b6b56a6ee0c8b17e25532665fc830a803ed3ea54
farah-salama <farahhsalamaa@gmail.com>
2023-12-29 02:12:39 +0200

    add analysis application

---
+++ src/main/java/com/example/social_network/AnalysisController.java
@@ -0,0 +1,34 @@
+package com.example.social_network;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class AnalysisController {
+    @FXML
+    private TextArea user1;
+    @FXML
+    private TextArea user2;
+    @FXML
+    private TextArea user_suggest;
+    @FXML
+    private Label output_label;
+
+    @FXML
+    protected void OnMostActiveButtonClick (ActionEvent event) {
+        
+    }
+    @FXML
+    protected void OnMostInfluentialButtonClick (ActionEvent event) {
+
+    }
+    @FXML
+    protected void OnMutualButtonClick (ActionEvent event) {
+
+    }
+    @FXML
+    protected void OnSuggestButtonClick (ActionEvent event) {
+
+    }
+}
